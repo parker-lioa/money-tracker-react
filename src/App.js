d699f7a70806fdffbc80582967ed8dfd1038dcb1
parker-lioa <joshf1558@gmail.com>
2020-08-29 13:31:41 +0800

    connet to server

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Cards from "./components/Cards";
-import getData from './Api/api';
+import getData from "./Api/api";
 
 const App = () => {
+  const set_data = async () => {
+    try {
+      const data = await getData();
+      if (data) {
+        setlist(data);
+      } else {
+        setlist([]);
+      }
 
-  const data = getData();
+      console.log(`${data} from set data! `);
+      console.log("set list successfully");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  const [list,setlist] = useState(data);
+  const init_list = () => {
+    const data = getData();
+
+    if (data) {
+      console.log(`${data} init`);
+      return data;
+    } else {
+      return [];
+    }
+
+    console.log(data);
+    console.log("set list successfully");
+  };
+
+  const [list, setlist] = useState([]);
+  const [query, setquery] = useState("ee");
+  const [status, setstatus] = useState("idle");
   const [left, setleft] = useState(500);
   const [cost, setcost] = useState(0);
   const [days, setdays] = useState(5);
-
   const [input, setinput] = useState(0);
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+
+    if (query === "ee") {
+      set_data();
+      setstatus("fetched");
+    }
+    setquery("e");
+  }, [query]);
 
   const addCost = (spend) => {
     setcost(spend + cost);
@@ -22,6 +62,7 @@ const App = () => {
   const addClick = () => {
     addCost(parseInt(input));
     setinput(0);
+    setquery('er');
   };
 
   return (
@@ -40,8 +81,19 @@ const App = () => {
             }}
           ></input>
           <button onClick={addClick}>Add</button>
+        </div>{" "}
+        <div className="list-container">
+          {status === "fetched"
+            ? list.map((item, index) => {
+                return (
+                  <div key={index} className="list-item">
+                    <div className="cost">item.cost</div>
+                    <div className="category">item.category</div>
+                  </div>
+                );
+              })
+            : ""}
         </div>
-        <div className="list-container"></div>
       </section>
     </div>
   );
